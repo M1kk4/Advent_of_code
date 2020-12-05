07c10c1bece908ae481283e1d3f47552c748e652
M1kk4 <74627639+M1kk4@users.noreply.github.com>
2020-12-05 14:23:05 +0100

    Update 1_day.py

--- 2020/1_day.py
+++ 2020/1_day.py
@@ -1,6 +1,5 @@
 # Part 1
 
-<pre><code>
 f = open("code.txt", "r")
 numbers = []
 for x in f:
@@ -12,11 +11,10 @@ for line in numbers:
         if line + bob == 2020:
             answer = line * bob
             print(str(answer))
-</pre></code>
 
 # Part 2
 
-<pre><code>
+
 f = open("code.txt", "r")
 numbers = []
 for x in f:
@@ -29,4 +27,4 @@ for line in numbers:
             if line + bob + KOBE == 2020:
                 answer = line * bob * KOBE
                 print(str(answer))            
-</pre></code>
+
